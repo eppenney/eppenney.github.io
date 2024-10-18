335da68b34a9c10ffa0b1c59326fc0bc7eea000a
Ethan P <ethanpenney2341@gmail.com>
2024-10-18 15:21:15 -0300

    Updated main page with header

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ document.addEventListener("DOMContentLoaded", function() {
         contactBtn.textContent = onOff ? "Close" : "Contact";
     });
 });
+
+const navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+const sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function handleScroll() {
+    if (window.pageYOffset > sticky) {
+        navbar.style.position = "fixed";
+        navbar.style.top = "0"; // Stick it to the top of the viewport
+    } else {
+        navbar.style.position = "relative";
+    }
+}
+
+// When the user scrolls the page, execute handleScroll
+window.onscroll = function() {
+    handleScroll();
+};
